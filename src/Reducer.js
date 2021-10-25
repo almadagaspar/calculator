bb98680ddbc2dffcf267c2112b6d4193de11d55b
Gaspar Almada <almada3d@gmail.com>
2021-10-25 14:12:15 -0300

    Testing push from Ubuntu 3

--- src/Reducer.js
+++ src/Reducer.js
@@ -4,6 +4,7 @@ import { NUEVO_NUMERO, OPERADOR, CAMBIAR_SIGNO, REINICIAR, RESULTADO } from './A
 // Testing push from Ubuntu 1
 // Testing push from Windows 1
 // Testing push from Ubuntu 2
+// Testing push from Ubuntu 3
 
 const initialState = {  // Creo un estado inicial.
     numA: 0,       // Estado que almacenará el primero número a ingresar.
