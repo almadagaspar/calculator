6bb542f58ddc9fad27d2e8f282c24c6c11dbafc7
Gaspar Almada <a.gaspar.it@gmail.com>
2022-05-03 23:41:53 -0300

    fix: refactorización de código.

--- src/Reducer.js
+++ src/Reducer.js
@@ -2,126 +2,96 @@ import { NUEVO_NUMERO, OPERADOR, CAMBIAR_SIGNO, REINICIAR, RESULTADO } from './A
 
 
 const initialState = {  // Creo un estado inicial.
-    numA: "0",       // Estado que almacenará el primero número a ingresar.
-    numB: "0",       // Estado que almacenará el segundo número a ingresar.
-    operador: 'SIN-DEFINIR',         // Estado que almacenará la operación matemática que se realizará entre los dos números ingresados al presionarse '='.
-    reemplazar: false         // Debo reemplazar el número mostrado por el nuevo número que se esta ingresando?
+	numA: "0",       // Estado que almacenará el primero número a ingresar.
+	numB: "0",       // Estado que almacenará el segundo número a ingresar.
+  operador: 'SIN-DEFINIR',    // Estado que almacenará la operación matemática que se realizará entre los dos números ingresados al presionarse '='.
+  reemplazar: false          // ¿ Debo reemplazar el número mostrado por el nuevo número que se esta ingresando?
 }
+	
 
 
 // El Reducer contempla el ingreso de un primer número, un operador matemático, un segundo número, y el boton '=' para generar el resultado. También contempla el cambio de signo del número, y reiniciar todos los valores.
 export default function Reducer(state = initialState, action) {
-    switch (action.type) {
-        case NUEVO_NUMERO:      // Si se presionó un botón con un número, o el boton con el punto...
+	switch (action.type) {
+		case NUEVO_NUMERO:      // Si se presionó un botón con un número, o el boton con el punto...
         
-        const screenNumber = document.getElementById("screenNumber").innerText;
+			const screenNumber = document.getElementById("screenNumber").innerText;
 
-            if ((action.payload === "." && screenNumber.includes('.')) ||   // Evito que se introduzca más de un punto...
-                (action.payload === "." && state.reemplazar === true) ||   // ... que se pueda introducir un punto tras obtener un resultado.
-                 screenNumber.length >= 11  ||                              // ... que el numero ingresado sea mayor que la pantalla.
-                (screenNumber === "0" && action.payload === "0") ){       // ... que se concatenen ceros como primer valor.
-                return { ...state }
-            }
+			if ((action.payload === "." && screenNumber.includes('.')) ||   // Evito que se introduzca más de un punto...
+				(action.payload === "." && state.reemplazar === true) ||     // ... que se pueda introducir un punto tras obtener un resultado.
+				screenNumber.length >= 11  ||                               // ... que el numero ingresado sea mayor que la pantalla.
+				(screenNumber === "0" && action.payload === "0") ){        // ... que se concatenen ceros como primer valor.
+					return { ...state }
+			}
 
-            // Si tengo que reemplazar el "0"
-            if (screenNumber === "0" && action.payload !== "." || 
-                state.reemplazar === true) {  
-                return state.operador === 'SIN-DEFINIR' ? {
-                    ...state,
-                    numA: action.payload,
-                    reemplazar: false  
-                } : {
-                    ...state,
-                    numB: action.payload,
-                    reemplazar: false  
-                }       
-            }
+			// Si tengo que reemplazar el "0"
+			if ((screenNumber === "0" && action.payload !== ".") || 
+					state.reemplazar === true) {  
+						return state.operador === 'SIN-DEFINIR' ? {
+							...state,
+							numA: action.payload,
+							reemplazar: false  
+						} : {
+							...state,
+							numB: action.payload,
+							reemplazar: false  
+						}       
+					}
             
-            // Si tengo que concatenar
-            return state.operador === 'SIN-DEFINIR' ? {
-                ...state,
-                numA: state.numA + action.payload 
-            } : {
-                ...state,
-                numB: state.numB + action.payload 
-            }
+					// Si tengo que concatenar
+					return state.operador === 'SIN-DEFINIR' ? {
+						...state,
+						numA: state.numA + action.payload 
+					} : {
+						...state,
+						numB: state.numB + action.payload 
+					}
                 
-        case OPERADOR:             // Si se presionó sobre un operador matemático...
-            return {
-                ...state,
-                operador: action.payload           // Lo almaceno en mi estado.
-            }
-        case CAMBIAR_SIGNO:      // Si se presionó el boton para cambiar el signo...
-            return state.operador === 'SIN-DEFINIR' ? {
-                ...state,
-                numA: (-Number(state.numA)).toString()            // ...entonces le tengo que cambiar el signo al segundo número.
-            } : {
-                ...state,
-                numB: (-Number(state.numB)).toString()            // ...entonces le tengo que cambiar el signo al segundo número.
-            }
-        case REINICIAR:          // Si se presionó el boton 'C' (Clear) reinicio todos los valores...
-            return {
-                ...state,
-                numA: state.numA = "0",
-                numB: state.numB = "0",
-                operador: state.operador = 'SIN-DEFINIR', 
-                reemplazar: state.reemplazar = false
-            };
-        case RESULTADO:     // Si se presionó el boton '=' relaizo la operación matemática correspondiente y almaceno el resultado en el primer número para que se pueda seguir haciendo más cálculos, y reinicio los otros estados.
-            switch (state.operador) {     
-                case '+':
-                    return {
-                        ...state,
-                        numA: (Number(state.numA) + Number(state.numB)).toString(),
-                        operador: state.operador = 'SIN-DEFINIR',
-                        numB: state.numB = "0",
-                        reemplazar: state.reemplazar = true
-                    }
+		case OPERADOR:             // Si se presionó sobre un operador matemático...
+			return {
+				...state,
+				operador: action.payload           // Lo almaceno en mi estado.
+			}
+		case CAMBIAR_SIGNO:      // Si se presionó el boton para cambiar el signo...
+			return state.operador === 'SIN-DEFINIR' ? {
+				...state,
+				numA: (-Number(state.numA)).toString()       // ...entonces le tengo que cambiar el signo al segundo número.
+			} : {
+				...state,
+				numB: (-Number(state.numB)).toString()       // ...entonces le tengo que cambiar el signo al segundo número.
+			}
+		case REINICIAR:            // Si se presionó el boton 'C' (Clear) reinicio todos los valores...
+			return resolveNewState(state, "0", false )
+		case RESULTADO:          // Si se presionó el boton '=' realizo la operación matemática correspondiente y almaceno el resultado en el primer número para que se pueda seguir haciendo más cálculos, y reinicio los otros estados.
+			switch (state.operador) {     
+				case '+':
+					return resolveNewState(state, (Number(state.numA) + Number(state.numB)).toString(), true )
+				case '-':
+					return resolveNewState(state, (Number(state.numA) - Number(state.numB).toString()), true )
+				case '*':
+					return resolveNewState(state, (Number(state.numA) * Number(state.numB).toString()), true )
+				case '/':
+					if (state.numB !== "0") {
+						return resolveNewState(state, (Number(state.numA) / Number(state.numB).toString()), true )
+					} else {
+						alert('Division by zero is impossible.')
+						return resolveNewState(state, "0" )
+					}
+				default:
+					return state;     // Siempre se debe devolver el estado para los casos en los que el Reducer no pueda procesar la Action recibida.
+			}
+		default:
+			return state;      // Siempre se debe devolver el estado para los casos en los que el Reducer no pueda procesar la Action recibida.
+	}
+}
 
-                case '-':
-                    return {
-                        ...state,
-                        numA: (Number(state.numA) - Number(state.numB)).toString(),
-                        operador: state.operador = 'SIN-DEFINIR',
-                        numB: state.numB = "0",
-                        reemplazar: state.reemplazar = true
-                    }
 
-                case '*':
-                    return {
-                        ...state,
-                        numA: (Number(state.numA) * Number(state.numB).toString()),
-                        operador: state.operador = 'SIN-DEFINIR',
-                        numB: state.numB = "0",
-                        reemplazar: state.reemplazar = true
-                    }
-
-                case '/':
-                    if (state.numB !== 0) {
-                        return {
-                            ...state,
-                            numA: (Number(state.numA) / Number(state.numB)).toString(),
-                            operador: state.operador = 'SIN-DEFINIR',
-                            numB: state.numB = "0",
-                            reemplazar: state.reemplazar = true
-                        }
-                    } else {
-                        alert('No se puede dividir por cero.')
-                        return {
-                            ...state,
-                            numA: "0",
-                            operador: state.operador = 'SIN-DEFINIR',
-                            numB: state.numB = "0",
-                            reemplazar: state.reemplazar = true
-                        }
-                    }
-
-                default:
-                    return state;     // Siempre se debe devolver el estado para los casos en los que el Reducer no pueda procesar la Action recibida.
-            }
-
-        default:
-            return state;      // Siempre se debe devolver el estado para los casos en los que el Reducer no pueda procesar la Action recibida.
-    }
-
+function resolveNewState (state, numA_value, reemplazarValue) {
+    return {
+            ...state,
+            numA: numA_value,
+            numB: "0",
+            operador: 'SIN-DEFINIR',
+            reemplazar: reemplazarValue
+        }
 }
